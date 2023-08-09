9aa3903fc2023acdc7a23a7e6c40b09274f15db1
matheusluis.dev@proton.me <matheusluis.dev@proton.me>
2023-08-09 14:35:26 -0300

    End of Section 2

---
+++ API/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(
+    option => option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception exception)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(exception, "An error occured during migration");
+}
+
+app.Run();
